6401983782363853cb098247cd4964047bf18426
ZhahaSy <69520767+ZhahaSy@users.noreply.github.com>
2023-02-06 11:46:50 +0800

    config:修改地址

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
 # git remote add origin https://github.com/sanyuan0704/my_blog.git
 
-git push -f origin  master:master
+git remote add origin https://github.com/ZhahaSy/my_blog.git
 
 cd ..
 
